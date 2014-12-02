6f53ddf2d873dd5e6d83bd19d8eaf22db85f3309
mpiffault <martin.piffault@gmail.com>
2014-12-03 00:49:17 +0100

    Ajout des auteurs + modulaire - 2

--- js/apastyle.js
+++ js/apastyle.js
@@ -1,1 +1,20 @@
-var u;
+var apaStyle = angular.module('apaStyle', ['filtres']);
+
+apaStyle.controller('ApaController', ['$scope', function($scope) {
+	$scope.auteurs = [
+	    {prenom:'', nom:''}];
+	$scope.ajouterAuteur = function () {
+	    $scope.auteurs.push({prenom:'', nom:''});
+	}
+
+	$scope.supprimerAuteur = function (id) {
+	    $scope.auteurs.remove(id);
+	}
+}]);
+
+var filtres = angular.module('filtres', []);
+filtres.filter('initiales', function() {
+    return function (nom) {
+	return nom.charAt(0).toUpperCase() + '.';
+    };
+});
